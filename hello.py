b0a02e1cdb92bb88ee5a6faad0e55dd056664d9f
Jsiyoon <jjc9090@naver.com>
2019-04-29 19:30:50 +0900

    git 사용방법 추가

--- hello.py
+++ hello.py
@@ -7,3 +7,10 @@ q.put((2,'C'))
 
 for i in range(q.qsize()):
     print(q.get())
+
+print("순서: git status")
+print("순서: git add .")
+print("순서: git status")
+print("순서: git commit -m 올리는 이름")
+print("순서: git branch")
+print("순서: git push -u origin master")
